ffe709024ab9ae9ab31450ce13d696c3f4a83d4b
Your Name <you@example.com>
2025-06-12 12:18:38 +0530

    routing addded

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,16 @@
 import Body from "./components/Body"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom"
+
 
 function App() {
   
   return (
     <>
+    
     <Header />
-    <Body />
+    <Outlet />
     <Footer/>
      
     </>
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Offers from './components/Offers.jsx';
+import Help from './components/Help.jsx';
+import Cart from './components/Cart.jsx';
+import SignIn from './components/SignIn.jsx';
+import Body from './components/Body.jsx';
+
+const appRoutes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Body />,
+        index: true
+      },
+      {
+        path: '/offers',
+        element: <Offers />
+      },
+      {
+        path: '/help',
+        element: <Help />
+      },
+      {
+        path: '/cart',
+        element: <Cart />
+      },
+      {
+        path: '/signIn',
+        element: <SignIn />
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRoutes} />
   </StrictMode>,
-)
+);
